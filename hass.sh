ebce08e81fbe7802976a54efc9175853e00c6463
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2020-09-29 12:35:03 -0400

    Changed git to https instead of ssh

--- hass.sh
+++ hass.sh
@@ -9,8 +9,8 @@ echo -e "Which dotfiles repo?\n    [1] Desktop\n    [2] Laptop"
 read input
 
 case "$input" in
-    1) dotfiles_repo="git@github.com:hisbaan/dotfiles" ;;
-    2) dotfiles_repo="git@github.com:hisbaan/dotfiles-laptop" ;;
+    1) dotfiles_repo="https://github.com/hisbaan/dotfiles" ;;
+    2) dotfiles_repo="https://github.com/hisbaan/dotfiles-laptop" ;;
 esac
 
 git clone "$dotfiles_repo" $HOME/hass/dotfiles
