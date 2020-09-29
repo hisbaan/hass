de87eaffbb83e28605dd0785a9f07d992d3329cc
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2020-09-29 12:08:22 -0400

    Added package install

--- hass.sh
+++ hass.sh
@@ -14,3 +14,31 @@ case "$input" in
 esac
 
 git clone "$dotfiles_repo" $HOME/hass/dotfiles
+
+[ ! -d $HOME/.config ] [ mkdir $HOME/.config ]
+mv $HOME/hass/dotfiles/.config/* $HOME/.config/
+
+[ ! -d $HOME/.local/bin ] [ mkdir -p $HOME/.local/bin ]
+mv $HOME/hass/dotfiles/.local/bin/* $HOME/.local/bin/
+
+[ ! -d $HOME/.xcolors ] [ mkdir -p $HOME/.xcolors ]
+mv $HOME/hass/dotfiles/.xcolors/* $HOME/.xcolors/
+
+[ ! -d $HOME/.icons ] [ mkdir -p $HOME/.icons ]
+mv $HOME/hass/dotfiles/.icons/* $HOME/.icons/
+
+[ ! -d $HOME/.themes ] [ mkdir -p $HOME/.themes ]
+mv $HOME/hass/dotfiles/.themes/* $HOME/.themes/
+
+[ ! -d $HOME/.doom.d ] [ mkdir -p $HOME/.doom.d ]
+mv $HOME/hass/dotfiles/.doom.d/* $HOME/.doom.d/
+
+mv $HOME/hass/dotfiles/.xinitrc $HOME/
+
+###########################
+### Installing packages ###
+###########################
+
+sudo pacman -Syu --needed < $HOME/hass/dotfiles/pkglist.txt
+
+# yay -S --needed < $HOME/hass/dotfiles/pkglist-aur.txt
