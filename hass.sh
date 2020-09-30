d56b17ac2cd6301605f975f9b913540c2e98e379
Hisbaan <34548959+hisbaan@users.noreply.github.com>
2020-09-30 11:53:20 -0400

    Added oh-my-zsh plugins, p10k, and yay

--- hass.sh
+++ hass.sh
@@ -13,27 +13,29 @@ case "$input" in
     2) dotfiles_repo="https://github.com/hisbaan/dotfiles-laptop" ;;
 esac
 
+sudo sh -c "echo 'ZDOTDIR=$HOME/.config/zsh' >> /etc/zsh/zshenv"
+
 git clone "$dotfiles_repo" $HOME/hass/dotfiles
 
 [ ! -d $HOME/.config ] && mkdir $HOME/.config
-mv $HOME/hass/dotfiles/.config/* $HOME/.config/
+cp -r $HOME/hass/dotfiles/.config/* $HOME/.config/
 
 [ ! -d $HOME/.local/bin ] && mkdir -p $HOME/.local/bin
-mv $HOME/hass/dotfiles/.local/bin/* $HOME/.local/bin/
+cp -r $HOME/hass/dotfiles/.local/bin/* $HOME/.local/bin/
 
 [ ! -d $HOME/.xcolors ] && mkdir -p $HOME/.xcolors
-mv $HOME/hass/dotfiles/.xcolors/* $HOME/.xcolors/
+cp -r $HOME/hass/dotfiles/.xcolors/* $HOME/.xcolors/
 
 [ ! -d $HOME/.icons ] && mkdir -p $HOME/.icons
-mv $HOME/hass/dotfiles/.icons/* $HOME/.icons/
+cp -r $HOME/hass/dotfiles/.icons/* $HOME/.icons/
 
 [ ! -d $HOME/.themes ] && mkdir -p $HOME/.themes
-mv $HOME/hass/dotfiles/.themes/* $HOME/.themes/
+cp -r $HOME/hass/dotfiles/.themes/* $HOME/.themes/
 
 [ ! -d $HOME/.doom.d ] && mkdir -p $HOME/.doom.d
-mv $HOME/hass/dotfiles/.doom.d/* $HOME/.doom.d/
+cp -r $HOME/hass/dotfiles/.doom.d/* $HOME/.doom.d/
 
-mv $HOME/hass/dotfiles/.xinitrc $HOME/
+cp $HOME/hass/dotfiles/.xinitrc $HOME/
 
 ###########################
 ### Installing packages ###
@@ -41,4 +43,18 @@ mv $HOME/hass/dotfiles/.xinitrc $HOME/
 
 sudo pacman -Syu --needed < $HOME/hass/dotfiles/pkglist.txt
 
-# yay -S --needed < $HOME/hass/dotfiles/pkglist-aur.txt
+git clone https://aur.archlinux.org/yay.git $HOME/yay
+cd $HOME/yay
+makepkg -si
+cd $HOME
+rm -rf yay
+
+yay -S --needed < $HOME/hass/dotfiles/pkglist-aur.txt
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zdharma/fast-syntax-highlighting.git $HOME/.config/zsh/oh-my-zsh/custom/plugins/fast-syntax-highlighting
+
+git clone https://github.com/zigius/expand-ealias.plugin.zsh $HOME/.config/zsh/oh-my-zsh/custom/plugins/expand-ealias
+echo "ealias sp='sudo pacman'" >> $HOME/.config/zsh/oh-my-zsh/custom/zsh-ealias.zsh
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.config/zsh/oh-my-zsh/custom/themes/powerlevel10k
