17f974f701227ab42cd50292dc66aa1a55771c5d
hisbaan <34548959+hisbaan@users.noreply.github.com>
2021-01-01 21:57:50 -0500

    Updated README.org, added backup, added .unison folder

--- hass.sh
+++ hass.sh
@@ -1,3 +1,5 @@
+#!/bin/env bash
+
 ###########################
 ### Installing Dotfiles ###
 ###########################
@@ -12,39 +14,61 @@ esac
 
 echo ""
 echo "${bold}Getting dotfiles"
+
 sudo sh -c "echo 'ZDOTDIR=$HOME/.config/zsh' >> /etc/zsh/zshenv"
 export ZDOTDIR=$HOME/.config/zsh
 
 git clone "$dotfiles_repo" $HOME/hass/dotfiles
 
-[ ! -d $HOME/.config ] && mkdir $HOME/.config
-cp -r $HOME/hass/dotfiles/.config/* $HOME/.config/
+# Backing up old configs.
+echo ""
+echo "${bold}Backing up current config to ~/hass-backup"
+mkdir -p $HOME/hass-backup
+[ -d $HOME/.config ]            && mv $HOME/.config               $HOME/hass-backup
+[ -d $HOME/.local/bin/scripts ] && mv $HOME/.local/bin/scripts    $HOME/hass-backup
+[ -d $HOME/.xcolors ]           && mv $HOME/.xcolors              $HOME/hass-backup
+[ -d $HOME/.icons ]             && mv $HOME/.icons                $HOME/hass-backup
+[ -d $HOME/.themes ]            && mv $HOME/.themes               $HOME/hass-backup
+[ -d $HOME/.doom.d ]            && mv $HOME/.doom.d               $HOME/hass-backup
+[ -d $HOME/.unison ]            && mv $HOME/.unison               $HOME/hass-backup
+[ -f $HOME/.xinitrc ]           && mv $HOME/.xinitrc              $HOME/hass-backup
 
-[ ! -d $HOME/.local/bin ] && mkdir -p $HOME/.local/bin
-cp -r $HOME/hass/dotfiles/.local/bin/* $HOME/.local/bin/
+# Install new configs.
+echo ""
+echo "${bold}Moving new config to correct location"
+DOT=$HOME/hass/dotfiles
 
-[ ! -d $HOME/.xcolors ] && mkdir -p $HOME/.xcolors
-cp -r $HOME/hass/dotfiles/.xcolors/* $HOME/.xcolors/
+mkdir $HOME/.config
+cp -r $DOT/.config/* $HOME/.config/
 
-[ ! -d $HOME/.icons ] && mkdir -p $HOME/.icons
-cp -r $HOME/hass/dotfiles/.icons/* $HOME/.icons/
+mkdir -p $HOME/.local/bin/scripts
+cp -r $DOT/.local/bin/scripts/* $HOME/.local/bin/scripts
 
-[ ! -d $HOME/.themes ] && mkdir -p $HOME/.themes
-cp -r $HOME/hass/dotfiles/.themes/* $HOME/.themes/
+mkdir -p $HOME/.xcolors
+cp -r $DOT/.xcolors/* $HOME/.xcolors/
 
-[ ! -d $HOME/.doom.d ] && mkdir -p $HOME/.doom.d
-cp -r $HOME/hass/dotfiles/.doom.d/* $HOME/.doom.d/
+mkdir -p $HOME/.icons
+cp -r $DOT/.icons/* $HOME/.icons/
 
-cp $HOME/hass/dotfiles/.xinitrc $HOME/
+mkdir -p $HOME/.themes
+cp -r $DOT/.themes/* $HOME/.themes/
+
+mkdir -p $HOME/.doom.d
+cp -r $DOT/.doom.d/* $HOME/.doom.d/
+
+mkdir -p $HOME/.unison
+cp -r $DOT/.unison/* $HOME/.unison/
+
+cp $DOT/.xinitrc $HOME/
 
 ###########################
 ### Installing packages ###
 ###########################
 
-# enable multilib repos in pacman
+# Enable multilib repos in pacman.
 sudo sh -c "sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf"
 
-# Installing and using reflector
+# Installing and using reflector.
 echo ""
 echo "${bold}Installing reflector"
 sudo pacman -S reflector
@@ -52,12 +76,12 @@ echo ""
 echo "${bold}Rating mirrors. This may take some time"
 sudo reflector --verbose -l 200 -n 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 
-# Installing packages from the default repos
+# Installing packages from the default repos.
 echo ""
 echo "${bold}Installing packages"
-sudo pacman -Syu --needed $(cat $HOME/hass/dotfiles/pkglist.txt)
+sudo pacman -Syu --needed $(cat $DOT/pkglist.txt)
 
-# Installing AUR helper
+# Installing AUR helper.
 echo ""
 echo "${bold}Installing paru (AUR helper)"
 git clone https://aur.archlinux.org/paru.git $HOME/paru
@@ -66,20 +90,23 @@ makepkg -si
 cd $HOME
 rm -rf paru
 
-# Installing AUR packages
+# Installing AUR packages.
 echo ""
 echo "${bold}Installing AUR packages"
-paru -S --needed < $HOME/hass/dotfiles/pkglist-aur.txt
+paru -S --needed < $DOT/pkglist-aur.txt
 
-# Installing ZSH plugins
+# Installing ZSH plugins.
 echo ""
 echo "${bold}Installing ZSH plugins"
 mkdir $HOME/.config/zsh/plugins
 git clone https://github.com/sharat87/zsh-vim-mode $HOME/.config/zsh/plugins/zsh-vim-mode
 git clone https://github.com/zdharma/fast-syntax-highlighting $HOME/.config/zsh/plugins/fast-syntax-hightlighting
 
-# Installing doom emacs
+# Installing doom emacs.
 echo ""
 echo "${bold}Installing doom emacs"
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 $HOME/.emacs.d/bin/doom install
+
+echo ""
+echo "Done! Enjoy the dots!" | figlet
